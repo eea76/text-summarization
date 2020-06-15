6e2f91ea72eb5a3818e4b0df59341837ee445b29
Elon <elon@Elons-iMac.local>
2020-06-15 16:03:05 -0700

    app structure

---
+++ project/app/api/ping.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+
+from app.config import get_settings, Settings
+
+router = APIRouter()
+
+
+@router.get('/ping')
+async def pong(settings: Settings = Depends(get_settings)):
+
+    obj = {
+        'ping': 'pong!',
+        'environment': settings.environment,
+        'testing': settings.testing
+    }
+
+    return obj
